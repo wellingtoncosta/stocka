91d64a914611fc5bbc05b2eb83ae0f236955d581
Wellington Costa <wellington.costa128@gmail.com>
2020-03-13 11:27:21 -0300

    Adding SwipeRefreshLayout and Kotlin Coroutines.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -72,8 +72,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
     implementation("androidx.recyclerview:recyclerview:1.1.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.0.0")
 
     implementation("com.google.android.material:material:1.1.0")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.70")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.4")
 }
