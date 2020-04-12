5e83146a96788fd846e11a5e502386796113c406
Wellington Costa <wellington.costa128@gmail.com>
2020-04-12 19:00:39 -0300

    Changing "Todos" to "Todo" in .sq file.

--- shared/src/commonMain/kotlin/io.github.wellingtoncosta.todoapp/data/Schema.kt
+++ shared/src/commonMain/kotlin/io.github.wellingtoncosta.todoapp/data/Schema.kt
@@ -3,7 +3,11 @@ package io.github.wellingtoncosta.todoapp.data
 import com.squareup.sqldelight.db.SqlDriver
 import io.github.wellingtoncosta.todoapp.TodoAppDatabase
 
-fun createQueryWrapper(driver: SqlDriver) = TodoAppDatabase(driver = driver)
+fun createQueryWrapper(driver: SqlDriver): TodoAppDatabase {
+    return TodoAppDatabase(
+        driver = driver
+    )
+}
 
 object Schema : SqlDriver.Schema by TodoAppDatabase.Schema {
     override fun create(driver: SqlDriver) {
