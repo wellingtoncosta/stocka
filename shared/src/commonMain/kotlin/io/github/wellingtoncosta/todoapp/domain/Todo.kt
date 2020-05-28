8edc40d9638d9daa1749ff3c7dfffbf8d14a8245
Wellington Costa <wellington.costa128@gmail.com>
2020-05-28 16:38:55 -0300

    Adding a new column for todo table which represents the ID of backend database.

--- shared/src/commonMain/kotlin/io/github/wellingtoncosta/todoapp/domain/Todo.kt
+++ shared/src/commonMain/kotlin/io/github/wellingtoncosta/todoapp/domain/Todo.kt
@@ -2,6 +2,7 @@ package io.github.wellingtoncosta.todoapp.domain
 
 data class Todo(
     val id: Int? = null,
+    val externalId: String? = null,
     val title: String,
     val description: String?,
     val status: TodoStatus = TodoStatus.PLANNED
